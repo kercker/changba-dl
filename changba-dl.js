fa2b02c756d17d677ec7661faf0bfd63cee0f282
kercker <minglangsi@163.com>
2017-08-29 21:34:51 +0800

    version 0.2. add download left to export

--- changba-dl.js
+++ changba-dl.js
@@ -1,7 +1,20 @@
-audio = document.getElementById('audio')['src']
-span = document.getElementById('export_song')
-p = span.parentElement
-a = document.createElement('a')
-a.setAttribute('href', audio)
-a.appendChild(document.createTextNode('下载'))
-span.append(a)
+function insertAfter(newNode, referenceNode) {
+    // code from: https://stackoverflow.com/questions/4793604/how-to-do-insert-after-in-javascript-without-using-a-library
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+audio = document.getElementById('audio')['src'];
+export_song = document.getElementById('export_song');
+em = document.createElement('em');
+i = document.createElement('i');
+span = document.createElement('span');
+
+a = document.createElement('a');
+a.setAttribute('href', audio);
+a.appendChild(document.createTextNode('下载'));
+
+em.appendChild(a);
+span.appendChild(i);
+span.appendChild(em);
+
+insertAfter(span, export_song);
